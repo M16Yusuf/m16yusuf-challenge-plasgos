f011ed0e96358b7b319aa5080a1b7a6b53b8caf4
M16yusuf <yusufsmd58@gmail.com>
2025-11-06 07:10:06 +0700

    refactor: update main entry point to use MyRouter and adjust import paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/styles/main.css";
 
-createRoot(document.getElementById('root')).render(
+// import App from "./App.jsx";
+import MyRouter from "./router.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MyRouter />
+  </StrictMode>
+);
